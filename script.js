4e2fae36d3b33252cd467cae302b96534842f783
speedydevv <57814581+speedydevv@users.noreply.github.com>
2022-02-22 10:03:08 +0300

    commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function gen() {
     min = document.getElementById("num1").value
     max = document.getElementById("num2").value
+    res = Math.floor(Math.random() * max) + min;
+    alert(res)
 }
